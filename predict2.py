63e411f4ad5a95800002b4697fd2ab83904146da
BootcampAI <57165250+Bootcamp-AI@users.noreply.github.com>
2024-10-18 23:52:18 -0500

    Add sagemaker deploy

--- predict2.py
+++ predict2.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
  
-model_file = 'model_C=1.0.bin'
+model_file = 'model_C=0.9.bin'
 
 with open(model_file, 'rb') as f_in:
     (dv, model) = pickle.load(f_in)
